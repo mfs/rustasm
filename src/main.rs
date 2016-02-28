f93bc5af41f6cad6ad162540f35c434a7b902425
Mike Sampson <mike@sambodata.com>
2016-02-28 15:16:18 +1100

    Add some operand types.
    To compile MOV we need to know the operand types. Thinking of keying
    the hash map using something like (mnemonic, OpCodeType). Other
    alternative would be to do the operand type logic in a method pointed
    to by the OpCode table entry. Would like to keep those methods as
    simple as possible though.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
 use std::collections::HashMap;
 
+enum OperandType {
+    Imm8,
+    Imm16,
+    Imm32,
+    Imm64,
+
+    Reg8,
+    Reg16,
+    Reg32,
+    Reg64,
+
+    Mem8,
+    Mem16,
+    Mem32,
+    Mem64,
+}
+
+enum OpCodeType {
+    None,
+    Type(OperandType, OperandType),
+}
+
 struct OpCode {
     code: Vec<u8>,
+    code_type: OpCodeType,
     length: usize
 }
 
@@ -18,7 +41,8 @@ impl Assembler {
             op_code_table: HashMap::new(),
         };
 
-        a.op_code_table.insert("syscall", OpCode {code: vec![0x0f, 0x05], length: 2});
+        a.op_code_table.insert("syscall",
+            OpCode {code: vec![0x0f, 0x05], code_type: OpCodeType::None, length: 2});
 
         a
     }
