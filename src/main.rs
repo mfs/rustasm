250d2ffa2520772d6b6ac333c93a6c1e87551d24
Mike Sampson <mike@sambodata.com>
2016-02-28 18:17:56 +1100

    Key hashmap with (mnemonic, operand type) for now.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::collections::HashMap;
 
+use OperandType::*;
+
+#[derive(PartialEq,Eq,Hash)]
 enum OperandType {
     Imm8,
     Imm16,
@@ -17,20 +20,26 @@ enum OperandType {
     Mem64,
 }
 
+#[derive(PartialEq,Eq,Hash)]
 enum OpCodeType {
     None,
     Type(OperandType, OperandType),
 }
 
+#[derive(PartialEq,Eq,Hash)]
+struct Mnemonic {
+    mnemonic: &'static str,
+    mnemonic_type: OpCodeType
+}
+
 struct OpCode {
     code: Vec<u8>,
-    code_type: OpCodeType,
     length: usize
 }
 
 struct Assembler {
     _location_counter: u64,
-    op_code_table: HashMap<&'static str, OpCode>,
+    op_code_table: HashMap<Mnemonic, OpCode>,
     //symbol_table
 }
 
@@ -41,16 +50,24 @@ impl Assembler {
             op_code_table: HashMap::new(),
         };
 
-        a.op_code_table.insert("syscall",
-            OpCode {code: vec![0x0f, 0x05], code_type: OpCodeType::None, length: 2});
+        a.op_code_table.insert(
+            Mnemonic { mnemonic: "syscall", mnemonic_type: OpCodeType::None },
+            OpCode {code: vec![0x0f, 0x05], length: 2}
+        );
+
+        a.op_code_table.insert(
+            Mnemonic { mnemonic: "mov", mnemonic_type: OpCodeType::Type(Reg32, Imm32) },
+            OpCode {code: vec![0xb8], length: 5}
+        );
 
         a
     }
 
-    fn list(&self, mnemonic: &str) {
-        let op = self.op_code_table.get(mnemonic).unwrap();
+    fn list(&self, mnemonic: Mnemonic) {
+        let op = self.op_code_table.get(&mnemonic).unwrap();
 
-        println!("mnemonic: {} | op_code: {:?} | length: {}", mnemonic, op.code, op.length);
+        println!("mnemonic: {} | op_code: {:?} | length: {}",
+                 mnemonic.mnemonic, op.code, op.length);
     }
 }
 
@@ -59,6 +76,7 @@ fn main() {
 
     let assembler = Assembler::new();
 
-    assembler.list("syscall");
+    assembler.list(Mnemonic{ mnemonic: "syscall", mnemonic_type: OpCodeType::None });
+    assembler.list(Mnemonic{ mnemonic: "mov", mnemonic_type: OpCodeType::Type(Reg32, Imm32) });
 
 }
