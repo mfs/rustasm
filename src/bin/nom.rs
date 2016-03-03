888f91983d7ebba4c2988f9097b2ded4a63ebb23
Mike Sampson <mike@sambodata.com>
2016-03-03 21:56:35 +1100

    Try parsing part of a source line.

--- src/bin/nom.rs
+++ src/bin/nom.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate nom;
 
+use std::str;
+use nom::{space, is_alphanumeric};
+
 named!( comment, preceded!( char!( ';' ), take_until!( b"\n" ) ) );
 
 named!( instruction, alt!( tag!( "mov" ) | tag!( "syscall" ) ) );
@@ -9,11 +12,41 @@ named!( label, take_while!( nom::is_alphanumeric ) );
 
 named!( operands, take_until_either!( b";\n" ) );
 
+#[derive(Debug)]
+struct Line<'a> {
+    label: Option<&'a str>,
+    instruction: Option<&'a str>,
+    operand: Option<String>,
+    comment: Option<String>
+}
+
+impl<'a> Line<'a> {
+    fn new(label: Option<&'a str>, instruction: Option<&'a str>) -> Line<'a> {
+        Line {
+            label: label,
+            instruction: instruction,
+            operand: None,
+            comment: None,
+        }
+    }
+
+}
+
+named!( line_asm<Line>,
+    chain!(
+        label: label ~
+        space? ~
+        i: instruction,
+        || { Line::new( Some(str::from_utf8(label).unwrap() ),
+                        Some(str::from_utf8(i).unwrap())) }
+    )
+);
+
 fn main() {
 
     let line = b"start mov    st1,st0         ; this sets st1 := st1 + st0\n";
 
-    let b = label(line);
+    let b = line_asm(line);
 
     println!("{:?}", b);
 
